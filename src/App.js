6054a8420974e3ae0dcd3f26a675924f86a4c68f
Varalakshmi <varalakshmi@srsofttech.in>
2022-09-06 15:28:06 +0530

    sill half

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Components/Header/Header'
+import Skillcontainer from './Components/Skillcontainer/Skillcontainer'
 import Topcontainer from './Components/Topcontainer/Topcontainer'
 const App = () => {
   return (
     <div>
       <Header />
       <Topcontainer />
+      <Skillcontainer />
     </div>
   )
 }
--- src/Components/Skillcontainer/Skillcontainer.js
+++ src/Components/Skillcontainer/Skillcontainer.js
@@ -1,9 +1,53 @@
 import React from 'react'
 import { Element } from 'react-scroll'
+import skills from "../Header/image/skills.jpg"
+import LinearProgress from "@material-ui/core/LinearProgress"
+
 const Skillcontainer = () => {
   return (
-    <Element>
-
+    <Element className="skillcontainer" id="skills">
+      <div className='skillcontaniner__image'>
+        <img src={skills} alt =""/>
+      </div>
+      <div className="skillcontainer__text">
+        <h2>SKILLSET</h2>
+        <div className="skillcontainer__skillset">
+         <h5>React</h5>
+         <div className="skillcontainer__slider skillcontainer__slider1">
+            <LinearProgress variant="determinate value{90}"/>
+         </div>
+        </div>
+        <div className="skillcontainer__skillset">
+         <h5>HTML</h5>
+         <div className="skillcontainer__slider skillcontainer__slider2">
+            <LinearProgress variant="determinate value{75}"/>
+         </div>
+        </div>
+        <div className="skillcontainer__skillset">
+         <h5>CSS</h5>
+         <div className="skillcontainer__slider skillcontainer__slider3">
+            <LinearProgress variant="determinate value{80}"/>
+         </div>
+        </div>
+        <div className="skillcontainer__skillset">
+         <h5>Jquery</h5>
+         <div className="skillcontainer__slider skillcontainer__slider4">
+            <LinearProgress variant="determinate value{70}"/>
+         </div>
+         <div className="skillcontainer__skillset">
+         <h5>GraphQl</h5>
+         <div className="skillcontainer__slider skillcontainer__slider5">
+            <LinearProgress variant="determinate value{50}"/>
+         </div>
+        </div>
+        <div className="skillcontainer__skillset">
+         <h5>React</h5>
+         <div className="skillcontainer__slider skillcontainer__slider6">
+            <LinearProgress variant="determinate value{60}"/>
+         </div>
+        </div>
+        </div>
+      </div>
     </Element>
   )
 }
