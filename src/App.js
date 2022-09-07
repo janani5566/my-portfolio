906a68e1830f3af17534654a8847ada2d087eca8
Varalakshmi <varalakshmi@srsofttech.in>
2022-09-07 16:50:45 +0530

    projects half

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from './Components/Header/Header'
+import Projectcontainer from './Components/Projectcontainer/Projectcontainer'
 import Skillcontainer from './Components/Skillcontainer/Skillcontainer'
 import Topcontainer from './Components/Topcontainer/Topcontainer'
+import "./App.css"
 const App = () => {
   return (
     <div>
       <Header />
       <Topcontainer />
       <Skillcontainer />
+      <Projectcontainer />
     </div>
   )
 }
---
+++ src/Components/Projectcontainer/Projectcontainer.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Element } from 'react-scroll'
+import Project1 from '../Project/Project1'
+const Projectcontainer = () => {
+ const projects = [
+    {
+        img:"https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-hand-drawn-learning-tools-book-education-background-image_353765.jpg",
+        title:"Facebook",
+        desc:"The more that you read, the more things you will know. The more that you learn, the more places you’ll go.",
+        link: "www.google.com",
+
+    },
+    {
+        img:"https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-hand-drawn-learning-tools-book-education-background-image_353765.jpg",
+        title:"Facebook",
+        desc:"The more that you read, the more things you will know. The more that you learn, the more places you’ll go.",
+        link: "www.google.com",
+    },
+    {
+        img:"https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-hand-drawn-learning-tools-book-education-background-image_353765.jpg",
+        title:"Facebook",
+        desc:"The more that you read, the more things you will know. The more that you learn, the more places you’ll go.",
+        link: "www.google.com",
+    },
+    {
+        img:"https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-hand-drawn-learning-tools-book-education-background-image_353765.jpg",
+        title:"Facebook",
+        desc:"The more that you read, the more things you will know. The more that you learn, the more places you’ll go.",
+        link: "www.google.com",
+    },
+    {
+        img:"https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-hand-drawn-learning-tools-book-education-background-image_353765.jpg",
+        title:"Facebook",
+        desc:"The more that you read, the more things you will know. The more that you learn, the more places you’ll go.",
+        link: "www.google.com",
+    },
+    {
+        img:"https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-hand-drawn-learning-tools-book-education-background-image_353765.jpg",
+        title:"Facebook",
+        desc:"The more that you read, the more things you will know. The more that you learn, the more places you’ll go.",
+        link: "www.google.com",
+    },
+
+ ]
+  return (
+    <Element className= "projectcontainer"id="projects">
+        <h1>projects</h1>
+        <p>here are some projects which I done for making lives of people easy.</p>
+
+  <div className="projectcontainer__projects">
+     {
+        projects.map((project,index)=>{
+            return (
+                <Project1
+                     key={index}
+                     img={project.img}
+                     title={project.title}
+                     desc={project.desc}
+                     link={project.link}/>
+            )
+        })
+    }
+</div>
+    </Element>
+  )
+}
+
+export default Projectcontainer
